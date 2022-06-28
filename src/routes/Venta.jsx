658e5f844fdb745d428257ef4acd0fd491bc49b0
Erika  Yazmin Dionisio Velasco <61323708+ErikaYazminDionisio@users.noreply.github.com>
2022-06-28 10:57:20 -0500

    compra1

--- src/routes/Venta.jsx
+++ src/routes/Venta.jsx
@@ -3,18 +3,7 @@ import React from 'react'
 const Venta = () => {
   return (
     <div className='container'>
-        <h5>Futbol</h5>
-        <p>Horarios</p>
-        <a href="#" className='text-primary'>Descargar Registro</a>
-        <h5>Basketball</h5>
-        <p>Horarios</p>
-        <a href="#" className='text-primary'>Descargar Registro</a>
-        <h5>Musica</h5>
-        <p>Horarios</p>
-        <a href="#" className='text-primary'>Descargar Registro</a>
-        <h5>Danza</h5>
-        <p>Horarios</p>
-        <a href="#" className='text-primary'>Descargar Registro</a>
+       
     </div>
   )
 }
